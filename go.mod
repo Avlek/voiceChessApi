5cd4f44493df77617dff944d21b3fbfea94f6144
avlek <yakiwigo@gmail.com>
2019-02-15 21:33:52 +0900

    hello world

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Avlek/voiceChessApi
+
+require github.com/julienschmidt/httprouter v1.2.0 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 )
 
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
 
-	log.Fatal(http.ListenAndServe(":80", router))
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
