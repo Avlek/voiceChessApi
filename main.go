06f49e3e266f817a73412aa7fdc9c8a9a957f768
avlek <yakiwigo@gmail.com>
2019-02-15 21:18:40 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+
+	log.Fatal(http.ListenAndServe(":80", router))
+}
